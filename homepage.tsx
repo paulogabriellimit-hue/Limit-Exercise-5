32280d676cf4504ff6c7fbeb6d48e71ff64a7366
paulogabriellimit-hue <paulogabriel.limit@hcdc.edu.ph>
2026-02-21 15:14:21 +0800

    Create homepage.tsx

---
+++ homepage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Homepage() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Welcome, Paulo!</Text>
+    </View>
+  );
+}
